5fd3a5223f91c524aa560a6c2ba6e726e811dc02
Dominik Richter <dominik.richter@gmail.com>
2012-07-07 06:53:55 -0700

    added support for 256 colors, redesigned 16 colors pattern, and added color detection on terminal
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

--- lib/zlog.rb
+++ lib/zlog.rb
@@ -1,33 +1,39 @@
 
 module Zlog
-  # @@pattern_info    = "-- %s"
-  # @@pattern_error   = "ee %s"
-  # @@pattern_warning = "ww %s"
-  # @@pattern_debug   = "dd %s"
-  # @@pattern_ok      = "++ %s"
 
-  @@pattern_info    = "\033[0m-- %s\033[0m"
-  @@pattern_error   = "\033[31mee %s\033[0m"
-  @@pattern_warning = "\033[33mww %s\033[0m"
-  @@pattern_debug   = "\033[37mdd %s\033[0m"
-  @@pattern_ok      = "\033[32m++ %s\033[0m"
+  STDOUT_PATTERN_NOCOLOR = {
+    :info     => "-- %s",
+    :error    => "ee %s",
+    :warning  => "ww %s",
+    :debug    => ".. %s",
+    :ok       => "++ %s"
+  }
 
-  # @@pattern_info    = "-- %s"
-  # @@pattern_error   = "ee %s"
-  # @@pattern_warning = "ww %s"
-  # @@pattern_debug   = "dd %s"
-  # @@pattern_ok      = "\033[38;5;046m++ %s\033[0m"
+  STDOUT_PATTERN_8COLORS = {
+    :info    => "\033[0m-- %s\033[0m",
+    :error   => "\033[1;31mee %s\033[0m",
+    :warning => "\033[1;33mww %s\033[0m",
+    :debug   => "\033[1;30m.. %s\033[0m",
+    :ok      => "\033[1;32m++ %s\033[0m",
+  }
 
+  STDOUT_PATTERN_256COLORS = {
+    :info    => "\033[38;5;255m-- %s\033[0m",
+    :error   => "\033[38;5;196mee %s\033[0m",
+    :warning => "\033[38;5;226mww %s\033[0m",
+    :debug   => "\033[38;5;241m.. %s\033[0m",
+    :ok      => "\033[38;5;046m++ %s\033[0m",
+  }
 
-  NOTHING         = 0
-  ERROR           = 20
-  OK              = 20
-  WARNING         = 40
-  INFO            = 50
-  VERBOSE         = 60
-  DEBUG           = 100
+  NOTHING   = 0
+  ERROR     = 20
+  OK        = 20
+  WARNING   = 40
+  INFO      = 50
+  VERBOSE   = 60
+  DEBUG     = 100
 
-  @@level           = WARNING
+  @@level   = WARNING
   
   def self.level=( v )
     @@level = v
@@ -38,23 +44,54 @@ module Zlog
   end
 
   def self.info(msg)
-    puts @@pattern_info % msg if @@level >= INFO
+    puts @@pattern[:info] % msg if @@level >= INFO
   end
 
   def self.error(msg)
-    puts @@pattern_error % msg if @@level >= ERROR
+    puts @@pattern[:error] % msg if @@level >= ERROR
   end
 
   def self.warning(msg)
-    puts @@pattern_warning % msg if @@level >= WARNING
+    puts @@pattern[:warning] % msg if @@level >= WARNING
   end
 
   def self.debug(msg)
-    puts @@pattern_debug % msg if @@level >= DEBUG
+    puts @@pattern[:debug] % msg if @@level >= DEBUG
   end
 
   def self.ok(msg)
-    puts @@pattern_ok % msg if @@level >= OK
+    puts @@pattern[:ok] % msg if @@level >= OK
   end
+
+  def self.initialize_stdout_colors()
+    @@pattern = self.get_stdout_pattern
+  end
+
+
+  private
+
+
+  TERMINALS_WITH_256_COLORS = ["gnome-terminal"]
+
+  def self.detect_terminal_colors()
+    col = `tput colors`.to_i
+    colorterm = ENV["COLORTERM"]
+    return 256 if TERMINALS_WITH_256_COLORS.include?(colorterm)
+    return col
+  end
+
+  def self.get_pattern_for_colors(colors)
+    return STDOUT_PATTERN_256COLORS if colors >= 256
+    return STDOUT_PATTERN_8COLORS if colors >= 8
+    STDOUT_PATTERN_NOCOLORS
+  end
+
+  def self.get_stdout_pattern()
+    self.get_pattern_for_colors self.detect_terminal_colors
+  end
+
+  @@pattern = get_stdout_pattern
+
+
 end
 
