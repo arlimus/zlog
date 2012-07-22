0671433c152c09283d0a30d7bc1e62d43925ea86
Dominik Richter <dominik.richter@gmail.com>
2012-07-22 08:14:54 -0700

    support continuous writing (very useful for progess indication)
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

--- lib/zlog.rb
+++ lib/zlog.rb
@@ -1,3 +1,4 @@
+require 'highline'
 
 module Zlog
 
@@ -37,29 +38,28 @@ module Zlog
   DEBUG     = 100
 
   @@level   = WARNING
-  
   def self.level=( v )
     @@level = v
   end
 
-  def self.info(msg)
-    puts @@pattern[:info] % msg if @@level >= INFO
+  def self.info(msg, continuous = false)
+    write @@pattern[:info] % msg, continuous if @@level >= INFO
   end
 
   def self.error(msg)
-    puts @@pattern[:error] % msg if @@level >= ERROR
+    write @@pattern[:error] % msg if @@level >= ERROR
   end
 
   def self.warning(msg)
-    puts @@pattern[:warning] % msg if @@level >= WARNING
+    write @@pattern[:warning] % msg if @@level >= WARNING
   end
 
-  def self.debug(msg)
-    puts @@pattern[:debug] % msg if @@level >= DEBUG
+  def self.debug(msg, continuous = false)
+    write @@pattern[:debug] % msg, continuous if @@level >= DEBUG
   end
 
   def self.ok(msg)
-    puts @@pattern[:ok] % msg if @@level >= OK
+    write @@pattern[:ok] % msg if @@level >= OK
   end
 
   def self.abort(msg)
@@ -82,6 +82,49 @@ module Zlog
 
 
   private
+
+  # true if the last line is a dirty-line i.e.
+  # if the the carriege point is at the beginning but
+  # the line still contains characters
+  # this typically happens in continuous mode
+  @@dirty_line = false
+  UNPRINTABLE_CHARACTERS = /(\e\[[^m]*m|[^[:print:]])/
+
+  # format contents so they fit into a certain width
+  # supports escaped characters
+  def self.format_line( width, contents )
+    s = 0
+    l = width - 1
+    m = UNPRINTABLE_CHARACTERS.match contents[s..l]
+    until m.nil?
+      s += m[0].length + m.begin(0)
+      l += m[0].length
+      m = UNPRINTABLE_CHARACTERS.match contents[s..l]
+    end
+    "%-#{l}s" % contents[0..l]
+  end
+
+  # format a message correctly in a continuous environment
+  # ie. lines that may have been written normally ("some\n")
+  #     and lines that are continuous ("at 92%\r")
+  # it returns a string that correctly behaves in the context
+  # of the current terminal
+  def self.format_continuous(msg, continuous = false)
+    w = HighLine::SystemExtensions.terminal_size[0]
+
+    if not continuous
+      m = ( @@dirty_line ) ? "%#{w-1}s\r%s\n" % ["",msg] : msg+"\n"
+      @@dirty_line = false
+      m
+    else
+      @@dirty_line = true
+      "%s\r" % format_line( w - 1 , msg )
+    end
+  end
+
+  def self.write(msg, continuous = false)
+    print format_continuous(msg, continuous)
+  end
 
 
   TERMINALS_WITH_256_COLORS = ["gnome-terminal"]
