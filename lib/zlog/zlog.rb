63261c6566a0459008f50ff36761a026085f87a7
Dominik Richter <dominik.richter@gmail.com>
2013-10-06 03:32:08 +0200

    bugfix: prevent duplicate entries of stdout appender + prevent removal of other appenders when initializing zlog stdout
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

--- lib/zlog/zlog.rb
+++ lib/zlog/zlog.rb
@@ -2,10 +2,17 @@ module Zlog
   VERSION = "0.6"
 
   def self.init_stdout opts = {named: false, loglevel: nil}
-    Logging.logger.root.appenders = Logging.appenders.stdout(
+    # configure the default stdout appender
+    Logging.appenders.stdout(
       level: opts[:loglevel],
       layout: (opts[:named]) ? Zlog::Layouts.named : Zlog::Layouts.simple
-      )
+    )
+    # find all non-stdout appenders
+    # this prevents duplicates of stdout appender
+    as = Logging.logger.root.appenders.find_all{|i| i.name != "stdout" }
+    # add back all appenders + our own stdout appender
+    Logging.logger.root.appenders = as + ["stdout"]
+    # finally set the log level if we have one configured
     Logging.logger.root.level = opts[:loglevel] if not opts[:loglevel].nil?
   end
 end
