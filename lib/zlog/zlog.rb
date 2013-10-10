292f062635a5dd20ad2c99f3258a24fdd3fdcf0a
Dominik Richter <dominik.richter@gmail.com>
2013-10-10 18:26:40 +0200

    feature: get a writable logfile from a list of candidates or generate a random one
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

--- lib/zlog/zlog.rb
+++ lib/zlog/zlog.rb
@@ -1,7 +1,8 @@
 module Zlog
   VERSION = "0.7"
+  extend self
 
-  def self.init_stdout opts = {named: false, loglevel: nil}
+  def init_stdout opts = {named: false, loglevel: nil}
     # configure the default stdout appender
     Logging.appenders.stdout(
       level: opts[:loglevel],
@@ -12,5 +13,31 @@ module Zlog
     as = Logging.logger.root.appenders.find_all{|i| i.name != "stdout" }
     # add back all appenders + our own stdout appender
     Logging.logger.root.appenders = as + ["stdout"]
+  end
+
+  def get_writable_logfile *candidates
+    ret = candidates.compact.find do |f|
+      # testing if something is writable, options:
+      # #1: discarged, returns false if the file doesn't exist;
+      #     it would require to check the folder too... :(
+      # File::writable? f
+      #2 trial+error
+      begin
+        File.open(f,"a").close
+        true
+      rescue
+        false
+      end
+    end
+    # in case none of the provided logfiles is writable, create one
+    if ret.nil?
+      # try to get a prefix as the filename from the first provided log file
+      prefix = File::basename( candidates[0].to_s )
+      # determine a random filename
+      File::join( Dir::tmpdir, Dir::Tmpname.make_tmpname( prefix, nil ) )
+    else
+      # return the found good filename
+      ret
+    end
   end
 end
--- test/classes/zlog_spec.rb
+++ test/classes/zlog_spec.rb
@@ -1,7 +1,33 @@
 require 'minitest_helper'
+require 'tmpdir'
 
 describe Zlog do
   it "can initialize itself to STDOUT" do
     Zlog.must_respond_to :init_stdout
+  end
+
+  describe 'get_writable_logfile' do
+
+    before :each do
+      @random_writable = File::join( Dir::tmpdir, Dir::Tmpname.make_tmpname( "", nil ) )
+      @random_non_writable = File::join( '/dev', Dir::Tmpname.make_tmpname("",nil) )
+    end
+
+    it "must give a working writable log file" do
+      Zlog.get_writable_logfile( @random_writable ).
+        must_equal @random_writable
+    end
+
+    it "must give return the first actually writable log file" do
+      Zlog.get_writable_logfile( @random_non_writable, @random_writable ).
+        must_equal @random_writable
+    end
+
+    it "must provide a random file if none of the provided candidates are writable" do
+      f = Zlog.get_writable_logfile( @random_non_writable, @random_writable )
+      f.must_be_instance_of String
+      f.wont_be_empty
+    end
+
   end
 end
